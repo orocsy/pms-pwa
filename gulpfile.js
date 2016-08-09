271593dc35d1f3f2c664a731ee9e44ca5591a0b5
Sean Cai <seancai@Seans-MacBook-Pro.local>
2016-08-09 13:54:51 -0500

    added gulp webserver and inject tasks into default task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
  */
 'use strict';
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+gulp.task('default',['generate-service-worker','webserver'],function () {
+    console.log('server started at 3400');
+});
 gulp.task('generate-service-worker', function(callback) {
     var path = require('path');
     var swPrecache = require('sw-precache');
@@ -12,4 +16,13 @@ gulp.task('generate-service-worker', function(callback) {
         staticFileGlobs:[rootDir + '/**/*.{js,css,html,png,jpg}'],
         stripPrefix: rootDir
     }, callback);
+});
+
+gulp.task('webserver', function() {
+    gulp.src('/app')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            port:3400
+        }));
 });
